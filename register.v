6c8622b63ed469fcac433eb36020488df87f76ad
arianaolson419 <arianaolson419@gmail.com>
2017-10-19 00:29:08 -0400

    Add 32 bit zero register module.

--- register.v
+++ register.v
@@ -16,6 +16,8 @@ input		clk
 
 endmodule // register
 
+// 32 bits of D Flip-Flops with enable.
+// Positive edge triggered
 module register32
 (
 output [31:0] q,
@@ -23,10 +25,27 @@ input[31:0] d,
 input wrenable, 
 input clk
 );
-	// TODO: figure out how to use register module here.
+
 	genvar i;
 	for (i = 0; i < 32; i = i + 1) begin
 		register singleRegister (q[i], d[i], wrenable, clk);
 	end
 
 endmodule // register32
+
+
+// 1 bit register with constant output 0.
+// Inputs are ignored.
+module register32zero 
+(
+output [31:0] q,
+input[31:0] d,
+input wrenable, 
+input clk
+);
+	genvar i;
+	for (i = 0; i < 32; i = i + 1) begin
+		assign q[i] = 1'b0;
+	end
+
+endmodule // register32zero
